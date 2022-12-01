fdb18eb3ccbf5a004994c3c628f990401cd33011
Alex <alexfil0606@gmail.com>
2022-12-01 21:04:03 +0300

    example01

---
+++ example01/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+
+
+
+int[] CreatrArrayInt(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите элемент массива ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+
+
+int[] arr = CreatrArrayInt(5);
+PrintArray(arr);
+int result = PositiveNumbers(arr);
+Console.Write($" -> {result}");
