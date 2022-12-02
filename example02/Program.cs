ef3b1bf63690e53a6509ab9f849ae432a0398bc6
Alex <alexfil0606@gmail.com>
2022-12-02 19:50:46 +0300

    example02

--- example02/Program.cs
+++ example02/Program.cs
@@ -20,8 +20,12 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение k2 ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 * x + b1;
+void IntersectionPoint(double b1, double k1, double b2, double k2)
+{
 
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
 
-Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+IntersectionPoint(b1, k1, b2, k2);
